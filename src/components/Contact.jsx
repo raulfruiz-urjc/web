24b12306782470a17eacc06dcf7c10f349fc7a59
Raulr00 <raul2000pro@gmail.com>
2026-02-11 11:57:17 +0000

    preuba captchaa 2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,17 +13,18 @@ export default function Contact() {
         setResult("Sending....");
         const formData = new FormData(event.target);
 
+        // ----- Enter your Web3 Forms Access key below---------
 
         formData.append("access_key", "06a9ba1b-ccd0-4965-8295-7436ccca9472");
 
         //const res = {
         //    success: true,
-        //    message: "Message sent successfully"
+       //     message: "Message sent successfully"
        // };
          const res = await fetch("https://api.web3forms.com/submit", {
              method: "POST",
              body: formData
-        }).then((res) => res.json());
+         }).then((res) => res.json());
 
         if (res.success) {
             console.log("Success", res);
@@ -91,10 +92,10 @@ export default function Contact() {
 
                     <input type="email" placeholder="Introduce tu correo" className="flex-1 px-3 py-2 focus:ring-1 outline-none border border-gray-300 dark:border-white/30 rounded-md bg-white dark:bg-darkHover/30" required name="email" />
                 </div>
-                <textarea rows="6" placeholder="Escribe tu mensaje..." className="w-full px-4 py-2 focus:ring-1 outline-none border border-gray-300 dark:border-white/30 rounded-md bg-white mb-6 dark:bg-darkHover/30" required name="message"></textarea>
+                <textarea rows="6" placeholder="Enter your message" className="w-full px-4 py-2 focus:ring-1 outline-none border border-gray-300 dark:border-white/30 rounded-md bg-white mb-6 dark:bg-darkHover/30" required name="message"></textarea>
                 <div className="h-captcha mb-6 max-w-full" data-captcha="true"></div>
                 <button type='submit' className="py-2 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border dark:border-white/30 dark:hover:bg-darkHover">
-                Enviar
+                Envoar
                     <img src="./assets/right-arrow-white.png" alt="" className="w-4" />
                 </button>
                 <p className='mt-4'>{result}</p>
